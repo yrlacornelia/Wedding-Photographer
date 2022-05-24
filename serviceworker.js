0a6d0f61d50952c6acca926c9b72b62c22d0730e
Yrla Hackzell <yrla.hackzell@hotmail.com>
2022-05-24 10:19:37 +0200

    service-worker

--- serviceworker.js
+++ serviceworker.js
@@ -1,3 +1,31 @@
-self.addEventListener('fetch', () => {
-    // Vi återkommer till detta nästa vecka
-   });
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('v1').then((cache) => {
+            return cache.addAll(['index.html', 'js/index.js', 'offline.html', 'favicon.ico', 'css/style-offline.css']);
+        })
+    );
+    self.skipWaiting();
+    console.log('Service worker installed!');
+});
+
+self.addEventListener('activate', (event) => {
+    self.skipWaiting();
+    console.log('Service worker activated!');
+});
+
+self.addEventListener('fetch', (event) => {
+    console.log(event.request.url); // Skriv ut url:en på varje nätverksförfrågan
+    if (navigator.onLine) {
+        console.log('Du är online!');
+    } else {
+        console.log('Du är offline!');
+        console.log('Begärd url:', event.request);
+        event.respondWith(
+            caches.match(event.request).then((response) => {
+                console.log('response', response);
+                if (response) return response;
+                else return caches.match(new Request('offline.html'));
+            })
+        );
+    }
+});
